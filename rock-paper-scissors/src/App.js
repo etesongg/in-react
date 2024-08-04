a3ad8efa8caa385814b44d85ffc55141ad5b75e1
etesong014 <etesong0114@gmail.com>
2024-08-05 01:50:02 +0900

    아이템 선택하기 - 리액트 함수 생성시 콜백형태로 전달 () => 함수() - <React.StrictMode>태그를 주석 처리

--- rock-paper-scissors/src/App.js
+++ rock-paper-scissors/src/App.js
@@ -2,7 +2,24 @@ import { useState } from 'react';
 import './App.css';
 import Box from "./component/Box"
 
+const choice = {
+  rock: {
+    name: "Rock",
+    img: "https://i.pinimg.com/564x/36/69/e9/3669e921fe419e00ca6dc3d274caeabb.jpg"
+  },
+  paper: {
+    name: "Paper",
+    img: "https://helloartsy.com/wp-content/uploads/kids/school/how-to-draw-paper/how-to-draw-paper-step-6.jpg"
+  },
+  scissors: {
+    name: "Rock",
+    img: "https://helloartsy.com/wp-content/uploads/kids/school/how-to-draw-scissors/how-to-draw-scissors-step-6.jpg"
+  }
+} 
 function App() {
+  const play = (userChoice) => {
+    console.log(userChoice,"choice")
+  }
   return (
     <div>
       <div className='main'>
@@ -10,9 +27,9 @@ function App() {
         <Box title="Computer"/>
       </div>
       <div className='main'>
-        <button>가위</button>
-        <button>바위</button>
-        <button>보</button>
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
       </div>
     </div>
     
--- rock-paper-scissors/src/component/Box.js
+++ rock-paper-scissors/src/component/Box.js
@@ -4,7 +4,7 @@ const Box = (props) => {
   return (
     <div className='box'>
       <h1>{props.title}</h1>
-      <img className='item-img' src='https://helloartsy.com/wp-content/uploads/kids/school/how-to-draw-scissors/how-to-draw-scissors-step-6.jpg'></img>
+      <img className='item-img' src=''></img>
       <h2>Win</h2>
     </div>
   )
