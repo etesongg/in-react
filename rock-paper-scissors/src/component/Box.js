94f67df42f24b70da2e0410a54d7636982045400
etesong014 <etesong0114@gmail.com>
2024-08-05 21:45:10 +0900

    승패에 따라 테두리색 다르게 보여주기 - ${} 사용

--- rock-paper-scissors/src/component/Box.js
+++ rock-paper-scissors/src/component/Box.js
@@ -7,8 +7,9 @@ const Box = (props) => {
   }else{
     result = props.result;
   }
+
   return (
-    <div className='box'>
+    <div className={`box ${result}`}>
       <h1>{props.title}</h1>
       <img className='item-img' src={props.item && props.item.img}></img>
       <h2>{result}</h2>
