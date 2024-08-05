82c7833d4196c2faa1fc4b13e3f404d88ce1b978
etesong014 <etesong0114@gmail.com>
2024-08-05 21:36:30 +0900

    유저, 컴퓨터 결과 반대로 보여주기

--- rock-paper-scissors/src/component/Box.js
+++ rock-paper-scissors/src/component/Box.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 const Box = (props) => {
+  let result;
+  if(props.title === "Computer" && props.result !== "Tie" && props.result !== ""){
+    result = props.result === "Win"? "Lose": "Win";
+  }else{
+    result = props.result;
+  }
   return (
     <div className='box'>
       <h1>{props.title}</h1>
       <img className='item-img' src={props.item && props.item.img}></img>
-      <h2>Win</h2>
+      <h2>{result}</h2>
     </div>
   )
 }
