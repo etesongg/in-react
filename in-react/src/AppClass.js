4918e3db7d0cd28eb076ca72e1c506841a1204ae
etesong014 <etesong0114@gmail.com>
2024-08-07 05:45:21 +0900

    컴포넌트 lifecycle - constructor - render - componentDidMount - componentDidUpdate - componentWillUnmount

--- in-react/src/AppClass.js
+++ in-react/src/AppClass.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react'
-import BoxClass from './component/BoxClass';
+import React, { Component } from "react";
+import BoxClass from "./component/BoxClass";
 
 export default class AppClass extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            counter2: 0,
-            num: 1,
-            value: 0
-        };
-    }
-    increase = () => {
-        this.setState({
-            counter2: this.state.counter2 + 1,
-            value: this.state.value + 1,
-        });
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter2: 0,
+      num: 1,
+      value: 0,
+    };
+    console.log("constructor"); // 1
+  }
+  increase = () => {
+    this.setState({
+      counter2: this.state.counter2 + 1,
+      value: this.state.value + 1,
+    });
+    console.log("increase", this.state); // counter2: 0, value: 0
+  };
+
+  componentDidMount() { // UI 세팅이 완료되면 알려줌
+    // api 콜
+    console.log("componentDidMount"); // 3
+  }
+
+  componentDidUpdate() { // state가 업데이트 되면 알려줌
+    console.log("componentDidUpdate", this.state); // counter2: 1, value: 1
+  }
   render() {
+    console.log("render"); // 2
     return (
       <div>
         <div>state: {this.state.counter2}</div>
-        <button onClick={this.increase}>클릭</button> 
-        <BoxClass num={this.state.value}/>
+        <button onClick={this.increase}>클릭</button>
+        {this.state.counter2 < 3 && <BoxClass num={this.state.value} />}
       </div>
-    )
+    );
   }
 }
--- in-react/src/component/BoxClass.js
+++ in-react/src/component/BoxClass.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 
 export default class BoxClass extends Component {
+  componentWillUnmount() { // 컴포넌트가 종료될때 실행 됨.
+    console.log("componentWillUnmount");
+  }
   render() {
     return <div>Box {this.props.num}</div>;
   }
